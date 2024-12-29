6097c7cfd97e0e97e319cb2ba85427e8ffd18a3c
rezch <dany1008@yandex.ru>
2024-12-29 21:23:33 +0300

    fix

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ import daho
 
 aho = daho.DAho(5)  # aho buckets count
 
-aho.insert('hello', 2)  # add 2 strings 'hello' to aho set
-aho.insert('abc')  # add one string 'abc' to aho set
+aho.insert('hello', 2)  # add 2 strings 'hello' to the aho set
+aho.insert('abc')  # add one string 'abc' to the aho set
 print(aho.request(text='hello world aabc'))  # count the inclusions of strings from the aho set in a text
 #> 3
 
@@ -11,14 +11,14 @@ aho.insert('a')
 print(aho.request('hello world aabc'))
 #> 5
 
-aho.insert('a', -1)  # add "-1" string 'hello' to aho set, same as delete
+aho.insert('a', -1)  # add "-1" string 'hello' to the aho set, еру same as delete 1 occurrence
 print(aho.request('hello world aabc'))
 #> 3
 
 print(aho.buckets)
 #> 5
 
-aho.resize(15)  # change aho buckets count
+aho.resize(15)  # changes the aho buckets count
 
 print(aho.buckets)
 #> 15
